d2291183b58eb1865bf9164549b694d36c58c174
mathematics128 <89085899+mathematics128@users.noreply.github.com>
2022-07-11 14:50:32 +0800

    Add files via upload

---
+++ classmates.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, name, birthday):
+        self.name = name
+        self.birthday = birthday
+
+LC = Student('卢灿', '08-08')
+
---
+++ happy.py
@@ -0,0 +1,16 @@
+﻿# 警告：以下代码仅为示例，可以运行，请随意尝试！
+
+from classmates import LC
+from random import randint
+from datetime import datetime
+
+def happy_birthday(person):
+    sentenses = ['生日快乐！', '天天开心！', '时来运转！', '长命百岁！', '你可爱死我了知道不']
+    sentense = person + '，' + sentenses[randint(0, 4)]
+    return sentense
+
+date = datetime.now().strftime('%m-%d')
+if date == LC.birthday:
+    for i in range(52):
+        print(happy_birthday(LC.name))
+
